83a5a15b84426c09d5f7e3ee6a9036655722263f
rch20 <reemshalak22@hotmail.com>
2022-09-11 23:50:52 +0300

    assignment done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //grab all the necessary DOM elements
+
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  const newTaskUl = document.getElementById("tasks");
+
+  newTaskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const newTaskDescription = document.getElementById("new-task-description");
+    const newTask = document.createElement("li");
+    newTask.innerText = newTaskDescription.value;
+
+    document.getElementById("tasks").appendChild(task);
+    event.target.reset();
+  });
 });
